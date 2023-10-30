ece47deca4740d2d5935bcabfbe34ecadec69e15
Pedro Noronha <pedro_noronha@outlook.com>
2023-10-30 09:55:16 -0300

    Incrementando herança, colocando a function Pertenco()

--- exercicio.js
+++ exercicio.js
@@ -28,6 +28,7 @@ function Heroi(nome, poder,habilidade,identidade, nomeDoGrupo){
 
 const heroi1 = new Heroi ('Batman', 'Dinheiro', 'detetive')
 const heroi2 = new Heroi('Homem Aranha', 'Teia', 'agilidade')
+const heroi3 = new Heroi('Hulk','Esmaga','Força')
 
 function Grupo(nomeDoGrupo){
     
@@ -40,14 +41,23 @@ function Grupo(nomeDoGrupo){
     this.setNomeDoGrupo = function(nomeDoGrupo){
         _nomeDoGrupo = nomeDoGrupo
     }
+
+    this.Pertenco = function() {
+        console.log(this.nome + ' diz : Faço parte do grupo ' + this.getNomeDoGrupo())
+    }
 }
 
-heroi1.setIdentidade('Bruce Wayne')
-heroi2.setIdentidade('Peter Parker')
-
-heroi1.setNomeDoGrupo('Liga da Justiça')
-heroi2.setNomeDoGrupo('Os Vingadores')
 
 
+heroi1.setIdentidade('Bruce Wayne');
+heroi2.setIdentidade('Peter Parker');
+heroi3.setIdentidade('Bruce Banner');
+
+heroi1.setNomeDoGrupo('Liga da Justiça');
+heroi2.setNomeDoGrupo('Os Vingadores');
+heroi3.setNomeDoGrupo('Os Vingadores');
+
+console.log(heroi2.revelaId())
+console.log(heroi2.Pertenco())
 
 
